f9431c53ffa0218b409c56ed942675c10ee01f6d
Muhammad Syahrul Bastomy <syahrulb13@gmail.com>
2019-12-26 07:57:14 +0700

    pembelajaran sudah sampai * penambahan Scaffold untuk merangkum body & AppBar * penambahan wiget AppBar * Penambahan wiget Body

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApps());
-}
+void main() => runApp(MyApps());
 
-class MyApps extends StatelessWidget{
-  Widget build(BuildContext context){
+class MyApps extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('hello'),
-      );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('my first apps'),
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Text('ini bagian body'),
+      ),
+    );
   }
 }
