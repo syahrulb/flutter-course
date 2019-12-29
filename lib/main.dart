81d625e2f1fefb7a44217de203d2ad7ea261f278
syahrul bastomy <syahrulb60@gmail.com>
2019-12-29 22:10:11 +0700

    course sudah smpai create wiget

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,47 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApps());
 
 class MyApps extends StatelessWidget {
+  var questionIndex = 0;
+  void answerQuestion() {
+    questionIndex = this.questionIndex + 1;
+    print(this.questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var pertanyaanKita = [
+      'pertanyaan 1',
+      'pertanyaan 2',
+    ];
+    List<String> listjawaban = [
+      'A',
+      'B',
+      'C',
+      'D',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('my first apps'),
           backgroundColor: Colors.redAccent,
         ),
-        body: Text('ini bagian body'),
+        body: Column(
+          children: [
+            Text(pertanyaanKita[this.questionIndex]),
+            RaisedButton(
+              child: Text(listjawaban[0]),
+              onPressed: this.answerQuestion,
+            ),
+            RaisedButton(
+              child: Text(listjawaban[1]),
+              onPressed: this.answerQuestion,
+            ),
+            RaisedButton(
+              child: Text(listjawaban[2]),
+              onPressed: this.answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
