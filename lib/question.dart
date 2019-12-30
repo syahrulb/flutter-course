b8e59994a760f78e23675b2e5db49993344c97a6
Muhammad Syahrul Bastomy <syahrulb13@gmail.com>
2019-12-30 12:53:43 +0700

    pembelajaran style di bagian question dart.

--- lib/question.dart
+++ lib/question.dart
@@ -5,6 +5,14 @@ class Question extends StatelessWidget {
   Question(this.questionText);
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
