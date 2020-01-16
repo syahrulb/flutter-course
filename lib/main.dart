aaaaaae3288b8e5d434b93aa5c7265a50f8f67cc
syahrul bastomy <syahrulb60@gmail.com>
2020-01-16 21:55:03 +0700

    pembelajaran tenary operator di body aplication.

--- lib/main.dart
+++ lib/main.dart
@@ -13,31 +13,34 @@ class MyApps extends StatefulWidget {
 }
 
 class _MyAppsState extends State<MyApps> {
+  final pertanyaanKita = const [
+    {
+      "pertanyaan": 'pertanyaan 1',
+      'answers': ['A', 'B', 'C']
+    },
+    {
+      "pertanyaan": 'pertanyaan 2',
+      'answers': ['A']
+    },
+    {
+      "pertanyaan": 'pertanyaan 3',
+      'answers': ['C']
+    },
+  ];
   var _questionIndex = 0;
   void _answerQuestion() {
     setState(() {
       _questionIndex = this._questionIndex + 1;
     });
     print(this._questionIndex);
+    if (_questionIndex < pertanyaanKita.length) {
+      print('we have more question');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var pertanyaanKita = [
-      {
-        "pertanyaan": 'pertanyaan 1',
-        'answers': ['A', 'B', 'C']
-      },
-      {
-        "pertanyaan": 'pertanyaan 2',
-        'answers': ['A']
-      },
-      {
-        "pertanyaan": 'pertanyaan 3',
-        'answers': ['C']
-      },
-    ];
-    // pertanyaanKita =[]; ini tidak bisa dilakukan ketika sebuah variable dijadikan const 
+    // pertanyaanKita =[]; ini tidak bisa dilakukan ketika sebuah variable dijadikan const
     var dummy = ["hollo"];
     dummy.add('max');
     print(dummy);
@@ -53,14 +56,20 @@ class _MyAppsState extends State<MyApps> {
           title: Text('my first apps'),
           backgroundColor: Colors.redAccent,
         ),
-        body: Column(
-          children: [
-            Question(pertanyaanKita[this._questionIndex]['pertanyaan']),
-            ...(pertanyaanKita[this._questionIndex]['answers'] as List <String>).map((jawaban){
-               return Answer(jawaban, _answerQuestion);
-            }).toList(),
-          ],
-        ),
+        body: _questionIndex < pertanyaanKita.length
+            ? Column(
+                children: [
+                  Question(pertanyaanKita[this._questionIndex]['pertanyaan']),
+                  ...(pertanyaanKita[this._questionIndex]['answers']
+                          as List<String>)
+                      .map((jawaban) {
+                    return Answer(jawaban, _answerQuestion);
+                  }).toList(),
+                ],
+              )
+            : Center(
+                child: Text('berhasil!!'),
+              ),
       ),
     );
   }
