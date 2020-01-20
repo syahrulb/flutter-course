56081a6f2f9e08fad45bd0cf5cf0b42fed4bd587
syahrul bastomy <syahrulb60@gmail.com>
2020-01-20 22:33:22 +0700

    peerbaikan score

--- lib/result.dart
+++ lib/result.dart
@@ -5,17 +5,16 @@ class Result extends StatelessWidget {
   Result(this.hasilSkor);
   String get resultPharse{
     var resultText = 'You did It.';
-    if (hasilSkor<= 8) {
+    if (hasilSkor>= 100) {
       resultText='You are awesome and inconcent';
-    } else if(hasilSkor <= 12){
+    } else if(hasilSkor >= 70){
        resultText = 'Pretty Likeable';
-    } else if (hasilSkor <= 16) {
+    } else if (hasilSkor >= 45) {
        resultText = 'you are ... Streng?';
     }
     else{
       resultText = 'you are suck mein.';
     }
-    print(hasilSkor);
     return resultText;
   }
   @override
