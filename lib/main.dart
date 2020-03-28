87edf7709fa3f0beeed36898e1a291b49a6051b9
syahrul bastomy <syahrulb60@gmail.com>
2020-03-28 21:23:33 +0700

    pembelajaran 28-03-2020 : 1. pembelajaran mengenai penggunaan column dan row. 2. pembelajaran di bagian syling di margin, border, dan padding. 3. pembelajarang styling container.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaksi.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,29 +7,83 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Simpan Pinjam',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('flutter App'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Card(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaksi> transaksis = [
+    Transaksi(
+        id: '1', judul: 'bayar kost', jumlah: 865000, tanggal: DateTime.now()),
+    Transaksi(
+        id: '2',
+        judul: 'beli beli mouse',
+        jumlah: 30000,
+        tanggal: DateTime.now()),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Aplikasi Simpan pinjam'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
               color: Colors.blue,
-              child: Container(
-                width: 100, 
-                child: Text('Chart!')
-                ),
+              child: Text('Grafik'),
               elevation: 5,
             ),
-            Card(
-              child: Text('list of tx'),
-            )
-          ],
-        ),
+          ),
+          Column(
+            children: transaksis.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      child: Text(
+                        tx.jumlah.toString(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple),
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2),
+                      ),
+                      padding: EdgeInsets.all(10),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.judul,
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          tx.tanggal.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
