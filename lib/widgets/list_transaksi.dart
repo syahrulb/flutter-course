ada4e445da15e001734f6870e068facaeeec3617
Syahrul Bastomy <syahrulb13@gmail.com>
2020-06-29 16:50:22 +0700

    belajar menggunakan ListView.builder daripada mengunakan ListView. hal ini dikarenakan optimasi data ketikan menampilkan banyak data list view.

--- lib/widgets/list_transaksi.dart
+++ lib/widgets/list_transaksi.dart
@@ -10,54 +10,53 @@ class ListTransaksi extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transaksis.map((transaksi) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Rp. ${transaksis[index].jumlah.toString()}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
                     ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'Rp. ${transaksi.jumlah.toString()}',
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transaksis[index].judul,
                       style: TextStyle(
+                        fontSize: 18,
                         fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
                       ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        transaksi.judul,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(transaksi.tanggal),
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                    Text(
+                      DateFormat.yMMMd().format(transaksis[index].tanggal),
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: transaksis.length,
       ),
     );
     ;
