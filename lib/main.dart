2477d35a94b727e65191c8aa7ce7aad2b4125d13
Syahrul Bastomy <syahrulb13@gmail.com>
2020-06-29 11:10:37 +0700

    yReturn To Beginning Of The Apocalypse

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,100 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
-void main(){
-  runApp(MyApps());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
 }
 
-class MyApps extends StatelessWidget{
-  Widget build(BuildContext context){
-    return MaterialApp(
-      home: Text('hello'),
-      );
+class MyHomePage extends StatelessWidget {
+  final List<Transaksi> transaksis = [
+    Transaksi(
+      id: '1',
+      judul: 'Sepatu baru',
+      jumlah: 69.98,
+      tanggal: DateTime.now(),
+    ),
+    Transaksi(
+      id: '2',
+      judul: 'Steam Wallet',
+      jumlah: 16.28,
+      tanggal: DateTime.now(),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Chart'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: transaksis.map((transaksi) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        transaksi.jumlah.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          transaksi.judul,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          transaksi.tanggal.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/transaction.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+class Transaksi {
+  final String id;
+  final String judul;
+  final double jumlah;
+  final DateTime tanggal;
+
+  Transaksi({
+    @required this.id,
+    @required this.judul,
+    @required this.jumlah,
+    @required this.tanggal,
+  });
+}
