36c4aa262e199c8728ca3ef355ce1fc55c3ccbad
Syahrul Bastomy <syahrulb13@gmail.com>
2020-06-29 13:12:40 +0700

    belajar menggunakan texteditingcontroller,  textField, Flatbutton, dan menambahkan plugin intl.dart di bagian pub.yaml dan main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 import './transaction.dart';
 
 void main() => runApp(MyApp());
@@ -28,15 +30,20 @@ class MyHomePage extends StatelessWidget {
       tanggal: DateTime.now(),
     ),
   ];
+  // String inputJudul;
+  // String inputJumlah;
+  final judulController = TextEditingController();
+  final jumlahController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Pengcatatan pengeluaran'),
+        backgroundColor: Colors.amber,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        // crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             width: double.infinity,
@@ -44,6 +51,31 @@ class MyHomePage extends StatelessWidget {
               color: Colors.blue,
               child: Text('Chart'),
               elevation: 5,
+            ),
+          ),
+          Card(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Judul'),
+                    controller: judulController,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Jumlah'),
+                    controller: jumlahController,
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      print(judulController.text);
+                      print(jumlahController.text);
+                    },
+                    child: Text('Tambah Transaksi'),
+                    textColor: Colors.purple,
+                  )
+                ],
+              ),
             ),
           ),
           Column(
@@ -64,7 +96,7 @@ class MyHomePage extends StatelessWidget {
                       ),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        transaksi.jumlah.toString(),
+                        'Rp. ${transaksi.jumlah.toString()}',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
@@ -83,7 +115,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          transaksi.tanggal.toString(),
+                          DateFormat.yMMMd().format(transaksi.tanggal),
                           style: TextStyle(color: Colors.grey),
                         ),
                       ],
